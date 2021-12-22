b0fec45ee10c41b0c2b83a997a73a5c95c7e6f82
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-22 14:28:22 +0900

    Jetpack - ViewModel 에 LiveData 추가

--- app/src/main/java/com/yeonnex/jetpackapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/yeonnex/jetpackapp/ui/main/MainViewModel.kt
@@ -1,18 +1,19 @@
 package com.yeonnex.jetpackapp.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() { // 뷰모델 상속
     private val usd_to_eu_rate = 0.74f
     private var dollarText = ""
-    private var result: Float = 0f
+    private var result: MutableLiveData<Float> = MutableLiveData()
 
     fun setAmount(value: String){
         this.dollarText = value
-        this.result = value.toFloat() * usd_to_eu_rate
+        this.result.value = value.toFloat() * usd_to_eu_rate
     }
 
-    fun getResult(): Float?{
+    fun getResult(): MutableLiveData<Float>{
         return result
     }
 }
