f317085712567f3ffbb4b8c55d0f59a91350dbbd
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-23 10:39:20 +0900

    Jetpack 내비게이션 아키텍쳐 컴포넌트 개요, 빌드 구성에 라이브러리 추가

--- app/src/main/java/com/yeonnex/jetpackapp/MainActivity.kt
+++ app/src/main/java/com/yeonnex/jetpackapp/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yeonnex.jetpackapp.ui.main.MainFragment
 
+// 내비게이션 아키텍쳐
+// NavController 클래스
+//  내비게이션 스택을 사용해서 사용자의 앱 내부 목적지 경로를 추작하여 화면 이동
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/yeonnex/jetpackapp/TestOwner.kt
+++ app/src/main/java/com/yeonnex/jetpackapp/TestOwner.kt
@@ -6,17 +6,21 @@ import androidx.lifecycle.LifecycleRegistry
 
 class TestOwner : LifecycleOwner {
     private val lifecycleRegistry: LifecycleRegistry
+
+    // 1. 자신의 인스턴스 참조로 초기화된 lifecycleRegistry 인스턴스가 필요
     init {
         lifecycleRegistry = LifecycleRegistry(this) // 지연 초기화같은 것!
+        // 4. TestObserver 클래스를 옵저버로 추가
+        lifecycle.addObserver(TestObserver())
     }
 
-    // LifecycleRegistry 의 인스턴스를 반환하는 함수
+    // 2. LifecycleRegistry 의 인스턴스를 반환하는 함수
     override fun getLifecycle(): Lifecycle {
         return lifecycleRegistry
     }
 
-    // 객체의 markState()함수 또는 handleLifecycleEvent() 함수를 사용해서
-    // 생명주기 이벤트를 발생
+    // 3. 객체의 markState()함수 또는 handleLifecycleEvent() 함수를 사용해서
+    //    생명주기 이벤트를 발생
     fun startOwner(){
         lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
     }
